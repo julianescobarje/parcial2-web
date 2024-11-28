19e2abba925f901e8834cd7a92c4a779be8f21cf
Julian Escobar <escobarjulian347@gmail.com>
2024-11-28 10:43:31 -0500

    Controlador vacio

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ClaseModule } from './clase/clase.module';
 import { BonoModule } from './bono/bono.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioEntity } from './usuario/usuario.entity';
+import { ClaseEntity } from './clase/clase.entity';
+import { BonoEntity } from './bono/bono.entity';
 
 @Module({
-  imports: [UsuarioModule, ClaseModule, BonoModule],
+  imports: [
+    UsuarioModule,
+    ClaseModule,
+    BonoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial',
+      entities: [UsuarioEntity, ClaseEntity, BonoEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/bono/bono.module.ts
+++ src/bono/bono.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BonoService } from './bono.service';
+import { BonoController } from './bono.controller';
 
 @Module({
-  providers: [BonoService]
+  providers: [BonoService],
+  controllers: [BonoController],
 })
 export class BonoModule {}

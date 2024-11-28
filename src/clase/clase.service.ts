b04b541f8f19eb66605b41b5921cd7c17c4a8991
Julian Escobar <escobarjulian347@gmail.com>
2024-11-28 10:35:52 -0500

    Lógica de clase

--- src/clase/clase.service.ts
+++ src/clase/clase.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ClaseEntity } from './clase.entity';
 
 @Injectable()
-export class ClaseService {}
+export class ClaseService {
+  constructor(
+    @InjectRepository(ClaseEntity)
+    private readonly claseRepository: Repository<ClaseEntity>,
+  ) {}
+
+  async crearClase(clase: ClaseEntity): Promise<ClaseEntity> {
+    const { codigo } = clase;
+
+    if (!codigo || codigo.length !== 10) {
+      throw new BadRequestException(
+        'El código debe tener exactamente 10 caracteres.',
+      );
+    }
+    const nuevaClase = this.claseRepository.create(clase);
+    return this.claseRepository.save(nuevaClase);
+  }
+
+  async findClaseById(id: string): Promise<ClaseEntity> {
+    const clase = await this.claseRepository.findOne({ where: { id } });
+
+    if (!clase) {
+      throw new NotFoundException('Clase no encontrada.');
+    }
+
+    return clase;
+  }
+}

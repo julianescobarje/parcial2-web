d16a9e14c1ebcf9480a64845faf2fbc5468a6cd1
Julian Escobar <escobarjulian347@gmail.com>
2024-11-30 11:04:26 -0500

    eslint

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
- const app = await NestFactory.create(AppModule);
- app.enableVersioning({
-   type: VersioningType.URI,
-   prefix: 'api/v',
-   defaultVersion: '1',
- });
- await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+  await app.listen(3000);
 }
 bootstrap();
